3406b9b2881479de0868815af496c11c1d98c698
Darya <darya_01@mail.ru>
2021-08-05 16:16:37 +0300

    Выполнено обязательное задание

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let isNumber = function(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+
+let play = function() {
+
+  let secretNumber = Math.floor(Math.random() * 100) + 1;
+
+  
+  let getAnswer = function() {
+    let userNumber = prompt('Угадай число от 1 до 100:');
+
+    if (userNumber === null) {
+      return;
+    } else if (!isNumber(userNumber)) {
+      alert('Введи число!');
+    } else if (userNumber > secretNumber) {
+      alert('Загаданное число меньше');
+    } else if (userNumber < secretNumber) {
+      alert('Загаданное число больше');
+    } else {
+      alert('Поздравляю, Вы угадали!!!');
+      return;
+    }
+
+    getAnswer();
+  };
+  
+  console.dir(getAnswer);
+
+  getAnswer();
+  
+
+};
+
+play();
+alert('Игра окончена');
